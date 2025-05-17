c5a3957c7007a75e6954ea7a7cb59bdbd05dfd95
rahe12 <144845749+rahe12@users.noreply.github.com>
2025-05-17 22:57:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -15,5 +15,5 @@ app.use('/api/auth', authRoutes);
 app.use('/api/expenses', expenseRoutes);
 app.use('/api/categories', categoryRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3600;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
